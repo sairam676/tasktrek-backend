468792322283c6aeaa0b91eb1503f910f61e34bf
sairam676 <sairamdevarasetty676@gmail.com>
2026-03-18 22:41:11 +0530

    feat: add user routes for auth and profile endpoints

---
+++ routes/userRoutes.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const { registerUser,loginUser,getMe } = require('../controllers/userController');
+const router = express.Router();
+const {protect}=require('../middlewear/authMiddlewear');
+router.post('/',registerUser);
+router.post('/login',loginUser);
+router.get('/me',protect,getMe);
+module.exports = router;
